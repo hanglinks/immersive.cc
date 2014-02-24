a37cbc22fb018fd27dfacadd2d334d02b4053e0d
Morgan Sutherland <morgan@msutherl.net>
2014-02-24 02:50:16 -0800

    improve styling, add JS test, webfonts

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
 console.log('This would be the main JS file.');
+
+$(document).ready(function() {
+	console.log("ready");
+
+	$('.grid').on('click', function(e) {
+		console.log(e);
+		$('body').append("<div class='project'</div>");
+	});	
+
+});
