a774d656da5b76c271832c0e2205a912620ae467
Morgan Sutherland <morgan@msutherl.net>
2014-04-03 08:47:16 -0700

    full-screen carousel implemented

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,38 +6,30 @@ $(document).ready(function() {
 	// 	$('body').append("<div class='project'</div>");
 	// });	
 
+	var setupSuperslides = function (element) {
+		console.log("\n-- Setup Superslides --");
+		console.log(element);
 
-	// Unslider
-	// --------
+		$(element).superslides();	
 
-	var setupUnslider = function (element) {
-
-		$('.project').unslider({
-			speed: 300,               //  The speed to animate each slide (in milliseconds)
-			delay: 9000000,              //  The delay between slide animations (in milliseconds)
-			complete: function() {},  //  A function that gets called after every slide animation
-			keys: true,               //  Enable keyboard (left, right) arrow shortcuts
-			dots: true,               //  Display dot navigation
-			fluid: true 							//  Support responsive design. May break non-responsive designs
-		});
-
-		var unslider = $(element).unslider();
+		$(element).superslides('next');
 
 		$('.next, .prev').click(function() {
-        var fn = this.className;
+	      var fn = this.className;
 				console.log('Clicked ' + fn);
-        unslider.data('unslider')[fn]();
-    });
+	      // $('.project').superslides.data('unslider')[fn]();
+	  });
 	}
+
 
 	// Featherlight 
 	// ------------
 
-	console.log("-- Featherlight --\n")
+	console.log("\n-- Featherlight --")
 
 
 	var featherlightConfig = function () {
-		console.log("-- Featherlight Config --\n");
+		console.log("\n-- Featherlight Config --");
 		$.featherlight.defaults =
 			{
 		    selector:     '[data-featherlight]',  /* elements that trigger the lightbox */
@@ -49,14 +41,14 @@ $(document).ready(function() {
 		    targetAttr:   'data-featherlight',    /* attribute of the triggered element that contains the selector to the lightbox content */
 		    openTrigger:  'click',                /* event that triggers the lightbox */
 		    closeTrigger: 'click',                /* event that triggers the closing of the lightbox */
-		    openSpeed:    250,                    /* duration of opening animation */
-		    closeSpeed:   250,                    /* duration of closing animation */
+		    openSpeed:    9999,                    /* duration of opening animation */
+		    closeSpeed:   9999,                    /* duration of closing animation */
 		    namespace:    'featherlight',         /* name of the events and css class prefix */
 		    resetCss:     false,                  /* reset all css */
 		    variant:      null,                   /* class that will be added to change look of the lightbox */
 		    closeOnBg:    true,                   /* close lightbox on click on the background */
 		    closeOnEsc:   true,                   /* close lightbox when pressing esc */
-		    // closeIcon:    'x',             				/* close icon */
+		    closeIcon:    'x',             				/* close icon */
 		    background:   null,                   /* custom DOM for the background, wrapper and the closebutton */
 		    autostart:    true,                   /* initialize all links with that match "selector" on document ready */
 		    open: function(event){                /* opens the lightbox "this" contains $instance with the lightbox, and with the config */
@@ -73,44 +65,7 @@ $(document).ready(function() {
 
 	$.featherlight.methods.open = function() {
 		featherlightOpen.apply(this, event); 
-		console.log("-- Unslider --\n");
-		setupUnslider('.project');
-		// imageResize();
+		setupSuperslides('.project');
 	}
-
-
-	var imageResize = function() {
-		$(".project .images img").each(function(){
-			console.log("-- Resize --\n");
-			console.log(this);
-		    // Uncomment the following if you need to make this dynamic
-		    var refH = $(window).height();
-		    var refW = $(window).width();
-		    var refRatio = refW/refH;
-
-		    console.log(refH);
-		    console.log(refW);
-		    console.log(refRatio);
-
-		    var imgH = $(this).height();
-		    var imgW = $(this).width();
-
-		    console.log(imgH);
-		    console.log(imgW);
-
-		    if ( (imgW/imgH) > refRatio ) { 
-		    		console.log("Greater than");
-		        $(this).css("height", refW + "px");
-		        $(this).css("width", "auto");
-		    } else {
-		    		console.log("Less than");
-		        $(this).css("height", "auto");
-		        $(this).css("width", "100%");
-		    }
-		})
-	}
-
-	// $(window).resize(imageResize);
-	// imageResize();
 
 });
